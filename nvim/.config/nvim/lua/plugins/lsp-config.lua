37ac770aa0e13d622b0b3571a3439fea8a6cab9a
Vinícius Barbosa <viniciuspinhob@gmail.com>
2026-01-29 11:09:58 -0300

    Refactor LSP configuration and set default spell language to en_us

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -5,38 +5,37 @@ require("mason-lspconfig").setup({
   automatic_installation = true,
 })
 
--- LSP configuration
-local lspconfig = require('lspconfig')
-
 -- Get capabilities from cmp_nvim_lsp (for autocompletion)
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
--- Common LSP settings
-local on_attach = function(client, bufnr)
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  
-  -- Key mappings
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'gv', '<cmd>vsplit | lua vim.lsp.buf.definition()<CR>', bufopts)
-  vim.keymap.set('n', 'gs', '<cmd>split | lua vim.lsp.buf.definition()<CR>', bufopts)
-  
-  -- NEW: Open definition in new tab
-  vim.keymap.set('n', 'gt', '<cmd>tab split | lua vim.lsp.buf.definition()<CR>', bufopts)
-  
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format({ async = true }) end, bufopts)
-end
+-- Global LSP config: capabilities for all servers (Neovim 0.11 vim.lsp.config)
+vim.lsp.config('*', {
+  capabilities = capabilities,
+})
 
--- Setup language servers
+-- Keymaps and buffer-local LSP setup on attach (replaces on_attach)
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('user-lsp-attach', { clear = true }),
+  callback = function(args)
+    local bufnr = args.buf
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'gv', '<cmd>vsplit | lua vim.lsp.buf.definition()<CR>', bufopts)
+    vim.keymap.set('n', 'gs', '<cmd>split | lua vim.lsp.buf.definition()<CR>', bufopts)
+    vim.keymap.set('n', 'gt', '<cmd>tab split | lua vim.lsp.buf.definition()<CR>', bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format({ async = true }) end, bufopts)
+  end,
+})
+
+-- Enable LSP servers (uses configs from nvim-lspconfig's lsp/*.lua when available)
 local servers = { 'pyright', 'ts_ls', 'lua_ls' }
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-  })
+for _, name in ipairs(servers) do
+  vim.lsp.enable(name)
 end
